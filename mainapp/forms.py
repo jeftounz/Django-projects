bf7efb5d1c818ee8cb88bfd38bbdea4295ee3374
jeftounz <srjesusc@gmail.com>
2024-02-26 21:41:59 -0400

    Wee ad css and html files , the url file and the modifications in the form and views file

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -5,3 +5,8 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username=forms.CharField()
     password =forms.CharField(widget=forms.PasswordInput)
+
+    
+
+
+    
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def dashboard(request):   #Main screen
+    return render(request,'registration/dashboard.html',{'section':'dashboard'})
+
+
+
